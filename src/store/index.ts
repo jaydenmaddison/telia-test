f51651ca7195d6456ff43dac5fce8ae3f4010462
Jayden Maddison <hayden@genflow.com>
2020-02-27 10:35:10 +0000

    setting up store

---
+++ src/store/index.ts
@@ -0,0 +1,34 @@
+import { combineReducers } from 'redux'
+import { all, fork } from 'redux-saga/effects'
+import { connectRouter, RouterState } from 'connected-react-router'
+import { History } from 'history'
+
+import { LayoutState, layoutReducer } from './layout'
+
+import videosSaga from './videos/sagas'
+import { VideosState } from './videos/types'
+import { videosReducer } from './videos/reducer'
+
+// The top-level state object
+export interface ApplicationState {
+  layout: LayoutState
+  videos: VideosState
+  router: RouterState
+}
+
+// Whenever an action is dispatched, Redux will update each top-level application state property
+// using the reducer with the matching name. It's important that the names match exactly, and that
+// the reducer acts on the corresponding ApplicationState property type.
+export const createRootReducer = (history: History) =>
+  combineReducers({
+    layout: layoutReducer,
+    videos: videosReducer,
+    router: connectRouter(history)
+  })
+
+// Here we use `redux-saga` to trigger actions asynchronously. `redux-saga` uses something called a
+// "generator function", which you can read about here:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/function*
+export function* rootSaga() {
+  yield all([fork(videosSaga)])
+}
